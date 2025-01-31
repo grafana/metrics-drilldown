fbed73a716269c3fc1cb5fa41f351003476821b2
Nick Richmond <nick.richmond@grafana.com>
2025-01-30 23:09:36 -0500

    test: configure e2e smoke tests

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -2,32 +2,13 @@ import { expect, test } from './fixtures';
 import { ROUTES } from '../src/constants';
 
 test.describe('navigating app', () => {
-  test('page Hello World should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.HelloWorld}`);
-    await expect(page.getByText('Hello world panel')).toBeVisible();
-  });
-
-  test('page With Tabs should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.WithTabs}`);
-    await expect(page.getByText('This scene showcases a basic tabs functionality.')).toBeVisible();
-  });
-
-  test('page Home should support an id parameter', async ({ gotoPage, page }) => {
+  test('home page should render successfully', async ({ gotoPage, page }) => {
     await gotoPage(`/${ROUTES.Home}`);
-    await expect(
-      page.getByText(
-        'This scene showcases a basic scene functionality, including query runner, variable and a custom scene object.'
-      )
-    ).toBeVisible();
+    await expect(page.getByText('Start your metrics exploration!')).toBeVisible();
   });
 
-  test('page With Drilldown should render sucessfully', async ({ gotoPage, page }) => {
-    // wait for page to successfully render
-    await gotoPage(`/${ROUTES.WithDrilldown}`);
-    await expect(
-      page.getByText(
-        'This scene showcases a basic drilldown functionality. Interact with room to see room details scene.'
-      )
-    ).toBeVisible();
+  test('trail page should render successfully', async ({ gotoPage, page }) => {
+    await gotoPage(`/${ROUTES.Trail}`);
+    await expect(page.getByText('Search metrics')).toBeVisible();
   });
 });
