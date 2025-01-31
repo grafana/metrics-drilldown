b08bc37e71fe7bcc49afd014d5076f65c49cf9fb
Nick Richmond <nick.richmond@grafana.com>
2025-01-30 22:20:32 -0500

    chore: remove config page

--- src/module.tsx
+++ src/module.tsx
@@ -2,8 +2,6 @@ import { AppPlugin, type AppRootProps } from '@grafana/data';
 import { LoadingPlaceholder } from '@grafana/ui';
 import React, { lazy, Suspense } from 'react';
 
-import { type AppConfigProps } from './App/AppConfig';
-
 const LazyApp = lazy(async () => {
   const { wasmSupported } = await import('./services/sorting');
   const { default: initOutlier } = await import('@bsull/augurs/outlier');
@@ -16,23 +14,10 @@ const LazyApp = lazy(async () => {
   return import('./App/App');
 });
 
-const LazyAppConfig = lazy(() => import('./App/AppConfig'));
-
 const App = (props: AppRootProps) => (
   <Suspense fallback={<LoadingPlaceholder text="" />}>
     <LazyApp {...props} />
   </Suspense>
 );
 
-const AppConfig = (props: AppConfigProps) => (
-  <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyAppConfig {...props} />
-  </Suspense>
-);
-
-export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
-  title: 'Configuration',
-  icon: 'cog',
-  body: AppConfig,
-  id: 'configuration',
-});
+export const plugin = new AppPlugin<{}>().setRootPage(App);
