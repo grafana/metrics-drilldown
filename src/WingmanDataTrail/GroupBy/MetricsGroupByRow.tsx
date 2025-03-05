7f216c387100a72fe6af9c295b239e8aee5bab9f
Brendan O'Handley <brendan.ohandley@grafana.com>
2025-03-04 19:16:03 -0600

    style the collapsible section label

--- src/WingmanDataTrail/GroupBy/MetricsGroupByRow.tsx
+++ src/WingmanDataTrail/GroupBy/MetricsGroupByRow.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
 import {
   PanelBuilders,
   SceneCSSGridItem,
@@ -7,7 +9,7 @@ import {
   type SceneObject,
   type SceneObjectState,
 } from '@grafana/scenes';
-import { CollapsableSection } from '@grafana/ui';
+import { CollapsableSection, useStyles2 } from '@grafana/ui';
 import React, { useState } from 'react';
 
 // Add new component interface
@@ -31,26 +33,21 @@ export class MetricsGroupByRow extends SceneObjectBase<MetricsGroupByRowState> {
   }
 
   public static Component = ({ model }: SceneComponentProps<MetricsGroupByRow>) => {
+    const styles = useStyles2(getStyles);
+
     const { groupName, groupType, body } = model.state;
     const [isCollapsed, setIsCollapsed] = useState(false);
     return (
       <>
-        {/* hide the body when collapsed */}
-        <CollapsableSection
-          onToggle={() => setIsCollapsed(!isCollapsed)}
-          label={
-            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <span>{`${groupName} ${groupType}`}</span>
-              {/* 
-                styling these button in the label so that they are far right needs to be done
-                The CollapsibleSection component does not allow for styling the label much 
-              */}
-              <button className="btn btn-sm btn-secondary">Include</button>
-              <button className="btn btn-sm btn-secondary">Exclude</button>
-            </div>
-          }
-          isOpen={!isCollapsed}
-        >
+        {/* for a custom label with buttons on the right, had to hack this above the collapsable section */}
+        <div className={styles.container}>
+          <span className={styles.groupName}>{`${groupName} ${groupType}`}</span>
+          <div className={styles.buttons}>
+            <button className="btn btn-sm btn-secondary">Include</button>
+            <button className="btn btn-sm btn-secondary">Exclude</button>
+          </div>
+        </div>
+        <CollapsableSection onToggle={() => setIsCollapsed(!isCollapsed)} label="" isOpen={!isCollapsed}>
           <body.Component model={body} />
         </CollapsableSection>
       </>
@@ -90,4 +87,22 @@ function createMetricPanel(title: string) {
   return new SceneCSSGridItem({
     body: PanelBuilders.timeseries().setTitle(title).setOption('legend', { showLegend: false }).build(),
   });
+}
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    container: css({
+      display: 'flex',
+      alignItems: 'center',
+      gap: '8px',
+      width: '98%',
+      marginBottom: '-42px',
+    }),
+    groupName: css({
+      fontSize: (theme.typography.fontSize = 26),
+    }),
+    buttons: css({
+      marginLeft: 'auto',
+    }),
+  };
 }
