380455a247f79a6dc249ac295b07465be310bb3e
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-11 16:45:21 -0600

    smoketests

--- tests/otelExperience.spec.ts
+++ tests/otelExperience.spec.ts
@@ -3,12 +3,14 @@ import { ROUTES } from '../src/constants';
 import { UI_TEXT } from '../src/constants/ui';
 
 test.describe('OTEL Experience', () => {
-  test('otel toggle is visible', async ({ gotoPage, getOtelSwitch }) => {
-    await gotoPage(`/${ROUTES.Trail}`);
-    await test.step('General assersions', async () => {
-      const otelSwitch = await getOtelSwitch();
+  test('otel toggle is visible', async ({ gotoPage, page }) => {
+    await test.step('navigate to trail', async () => {
+      await gotoPage(`/${ROUTES.Trail}`);
+    });
+
+    await test.step('element is visible', async () => {
+      const otelSwitch = page.getByLabel(UI_TEXT.METRIC_SELECT_SCENE.OTEL_LABEL);
       await expect(otelSwitch).toBeVisible();
-      await expect(otelSwitch).toContainText(UI_TEXT.METRIC_SELECT_SCENE.OTEL_LABEL);
     });
   });
 });
