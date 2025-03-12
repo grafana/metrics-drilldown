99b425c70ffab6bf2a4032ebbd6f1d8bb1969e70
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-12 08:34:14 -0600

    check for the deployment to be on

--- tests/otelExperience.spec.ts
+++ tests/otelExperience.spec.ts
@@ -3,14 +3,26 @@ import { ROUTES } from '../src/constants';
 import { UI_TEXT } from '../src/constants/ui';
 
 test.describe('OTEL Experience', () => {
-  test('otel toggle is visible', async ({ gotoPage, page }) => {
+  test('otel toggle workflow', async ({ gotoPage, page }) => {
     await test.step('navigate to trail', async () => {
       await gotoPage(`/${ROUTES.Trail}`);
     });
 
+    const otelSwitch = page.getByLabel(UI_TEXT.METRIC_SELECT_SCENE.OTEL_LABEL);
+
     await test.step('element is visible', async () => {
-      const otelSwitch = page.getByLabel(UI_TEXT.METRIC_SELECT_SCENE.OTEL_LABEL);
       await expect(otelSwitch).toBeVisible();
+    });
+
+    await test.step('turning otel experience on', async () => {
+      await expect(otelSwitch).toBeVisible();
+      await expect(otelSwitch).not.toBeChecked();
+      await otelSwitch.check({ force: true });
+      await expect(otelSwitch).toBeChecked();
+    });
+
+    await test.step('asssert deployment_environment filter is on', async () => {
+      await expect(page.getByText('deployment_environment')).toBeVisible();
     });
   });
 });
