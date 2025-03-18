3125c497342c6d8b580beb98b57fe2ef0c9d623c
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-03-18 13:16:03 +0100

    test(E2E): Fix test runtime error

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,19 @@
-import { VariantVariable } from 'WingmanOnboarding/VariantVariable';
-
 import pluginJson from './plugin.json';
 
 export const PLUGIN_BASE_URL = `/a/${pluginJson.id}`;
 
+// we have to duplicate this definition from 'WingmanOnboarding/VariantVariable' here
+// to prevent the E2E tests to fail with "ReferenceError: window is not defined"
+// it happens in runtime because of the `import { CustomVariable } from '@grafana/scenes';` :man_shrug:
+const VARIANT_VARIABLE_OPTIONS = ['onboard-filters-sidebar', 'onboard-filters-pills'];
+
 export const ROUTES = {
   Home: '',
   Trail: 'trail',
-  OnboardWithSidebar: VariantVariable.OPTIONS[0],
-  OnboardWithPills: VariantVariable.OPTIONS[1],
-  TrialWithSidebar: VariantVariable.OPTIONS[0].replace('onboard', 'trail'),
-  TrialWithPills: VariantVariable.OPTIONS[1].replace('onboard', 'trail'),
+  OnboardWithSidebar: VARIANT_VARIABLE_OPTIONS[0],
+  OnboardWithPills: VARIANT_VARIABLE_OPTIONS[1],
+  TrialWithSidebar: VARIANT_VARIABLE_OPTIONS[0].replace('onboard', 'trail'),
+  TrialWithPills: VARIANT_VARIABLE_OPTIONS[1].replace('onboard', 'trail'),
 };
 
 export const DATASOURCE_REF = {
