3591ba0b5cb0ee8c10fdb9bf497d87d043eb537c
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-19 17:39:57 -0600

    remove otel for now

--- e2e/tests/trailView/trailView.spec.ts
+++ e2e/tests/trailView/trailView.spec.ts
@@ -13,11 +13,11 @@ test.describe('Smoke tests', () => {
     await expect(trailView.getByText('View by')).toBeVisible();
     await expect(trailView.getByText('All metric names')).toBeVisible();
 
-    await trailView.assertSelectedTimeRange('2025-02-18 14:30:00 to 2025-02-18 17:15:00');
+    await trailView.assertSelectedTimeRange('2025-02-18 07:30:00 to 2025-02-18 10:15:00');
 
     await expect(trailView.getByTestId('scene-body')).toBeVisible();
 
     const panelsCount = await trailView.getByTestId('scene-body').locator('[data-viz-panel-key]').count();
-    await expect(panelsCount).toBe(20);
+    expect(panelsCount).toBe(20);
   });
 });
