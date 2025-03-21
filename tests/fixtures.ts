79cc5d5225d540b177a2e993ef20361637476557
Miguel Palau <mpalauzarza@gmail.com>
2025-03-21 13:17:01 -0600

    chore: otel experience e2e tests (#166)
    * wiring of docker helper
    
    * first attempt at the test
    
    * smoketests
    
    * check for the deployment to be on
    
    * complete otel workflow
    
    * also check for the attribute
    
    * moving to fixtures
    
    * a bit more generic metric panel grabber

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,10 +1,16 @@
 import { test as base, type AppConfigPage, type AppPage } from '@grafana/plugin-e2e';
+import { type Locator } from '@playwright/test';
 
+import { ROUTES } from '../src/constants';
+import { UI_TEXT } from '../src/constants/ui';
 import pluginJson from '../src/plugin.json';
 
 type AppTestFixture = {
   appConfigPage: AppConfigPage;
   gotoPage: (path?: string) => Promise<AppPage>;
+  otelSwitch: Locator;
+  navigateToTrail: () => Promise<void>;
+  getMetricPanel: (title: string) => Promise<Locator>;
 };
 
 export const test = base.extend<AppTestFixture>({
@@ -21,6 +27,20 @@ export const test = base.extend<AppTestFixture>({
         pluginId: pluginJson.id,
       })
     );
+  },
+  otelSwitch: async ({ page }, use) => {
+    const switchElement = page.getByLabel(UI_TEXT.METRIC_SELECT_SCENE.OTEL_LABEL);
+    await use(switchElement);
+  },
+  navigateToTrail: async ({ gotoPage }, use) => {
+    await use(async () => {
+      await gotoPage(`/${ROUTES.Trail}`);
+    });
+  },
+  getMetricPanel: async ({ page }, use) => {
+    await use(async (title: string) => {
+      return page.getByTestId(`data-testid Panel header ${title}`);
+    });
   },
 });
 
---
+++ tests/otelExperience.spec.ts
@@ -0,0 +1,34 @@
+import { expect, test } from './fixtures';
+
+test.describe('OTEL Experience', () => {
+  test('otel enabled workflow', async ({ navigateToTrail, otelSwitch, getMetricPanel, page }) => {
+    await test.step('navigate to trail', async () => {
+      await navigateToTrail();
+    });
+
+    await test.step('element is visible', async () => {
+      await expect(otelSwitch).toBeVisible();
+    });
+
+    await test.step('turning otel experience on', async () => {
+      await expect(otelSwitch).toBeVisible();
+      await expect(otelSwitch).not.toBeChecked();
+      await otelSwitch.check({ force: true });
+    });
+
+    await test.step('assert deployment_environment filter is on', async () => {
+      await expect(otelSwitch).toBeChecked();
+      await expect(page.getByText('deployment_environment = prod')).toBeVisible();
+    });
+
+    await test.step('select utf8 metrics', async () => {
+      const metricPanel = await getMetricPanel('a.utf8.metric 🤘');
+      await metricPanel.getByRole('button', { name: 'select' }).click();
+      expect(page.url().includes('otel_and_metric_filters')).toBeTruthy();
+    });
+
+    await test.step('confirm utf8 attribute is present', async () => {
+      expect(page.getByRole('link', { name: 'resource ę' })).toBeTruthy();
+    });
+  });
+});
