baf6e745c6cf2fec4afe1e2005f01917dc55842d
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-03-26 10:56:07 +0100

    fix(MetricsWithLabelValueVariable): Add support for sort by

--- src/WingmanDataTrail/GroupBy/MetricsWithLabelValue/MetricsWithLabelValueVariable.ts
+++ src/WingmanDataTrail/GroupBy/MetricsWithLabelValue/MetricsWithLabelValueVariable.ts
@@ -1,10 +1,18 @@
 import { VariableHide, VariableRefresh } from '@grafana/data';
-import { QueryVariable, sceneGraph, type MultiValueVariable } from '@grafana/scenes';
+import { QueryVariable, sceneGraph, type CustomVariable, type MultiValueVariable } from '@grafana/scenes';
+import { isEqual } from 'lodash';
 
 import { VAR_FILTERS } from 'shared';
+import { EventSortByChanged } from 'WingmanDataTrail/HeaderControls/MetricsSorter/EventSortByChanged';
+import {
+  MetricsSorter,
+  VAR_WINGMAN_SORT_BY,
+  type SortingOption,
+} from 'WingmanDataTrail/HeaderControls/MetricsSorter/MetricsSorter';
 import { EventQuickSearchChanged } from 'WingmanDataTrail/HeaderControls/QuickSearch/EventQuickSearchChanged';
 import { QuickSearch } from 'WingmanDataTrail/HeaderControls/QuickSearch/QuickSearch';
 import { MetricsVariableFilterEngine } from 'WingmanDataTrail/MetricsVariables/MetricsVariableFilterEngine';
+import { MetricsVariableSortEngine } from 'WingmanDataTrail/MetricsVariables/MetricsVariableSortEngine';
 import { EventFiltersChanged } from 'WingmanDataTrail/SideBar/EventFiltersChanged';
 import { SideBar } from 'WingmanDataTrail/SideBar/SideBar';
 
@@ -14,6 +22,7 @@ export const VAR_METRIC_WITH_LABEL_VALUE = 'metrics-with-label-value';
 
 export class MetricsWithLabelValueVariable extends QueryVariable {
   private filterEngine: MetricsVariableFilterEngine;
+  private sortEngine: MetricsVariableSortEngine;
 
   constructor({ labelName, labelValue }: { labelName: string; labelValue: string }) {
     super({
@@ -32,6 +41,7 @@ export class MetricsWithLabelValueVariable extends QueryVariable {
     });
 
     this.filterEngine = new MetricsVariableFilterEngine(this as unknown as MultiValueVariable);
+    this.sortEngine = new MetricsVariableSortEngine(this as unknown as MultiValueVariable);
 
     this.addActivationHandler(this.onActivate.bind(this, labelName, labelValue));
   }
@@ -91,6 +101,34 @@ export class MetricsWithLabelValueVariable extends QueryVariable {
       });
     } catch (error) {
       console.warn('SideBar not found - no worries, gracefully degrading...', error);
+    }
+
+    // wrapped in a try/catch to support the different variants / prevents runtime errors in WingMan's onboarding screen
+    try {
+      const metricsSorter = sceneGraph.findByKeyAndType(this, 'metrics-sorter', MetricsSorter);
+      const sortByVariable = metricsSorter.state.$variables.getByName(VAR_WINGMAN_SORT_BY) as CustomVariable;
+
+      this._subs.add(
+        metricsSorter.subscribeToEvent(EventSortByChanged, (event) => {
+          this.sortEngine.sort(event.payload.sortBy);
+        })
+      );
+
+      this._subs.add(
+        this.subscribeToState((newState, prevState) => {
+          if (newState.loading === false && prevState.loading === true) {
+            this.sortEngine.sort(sortByVariable.state.value as SortingOption);
+            return;
+          }
+
+          if (!isEqual(newState.options, prevState.options)) {
+            this.sortEngine.sort(sortByVariable.state.value as SortingOption);
+            return;
+          }
+        })
+      );
+    } catch (error) {
+      console.warn('MetricsSorter not found - no worries, gracefully degrading...', error);
     }
   }
 }
