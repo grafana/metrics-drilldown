e84820f23c19a38144523b0971ebc99434622826
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-31 12:51:39 -0600

    remove unused imports

--- e2e/fixtures/index.ts
+++ e2e/fixtures/index.ts
@@ -1,11 +1,9 @@
 import { test as base, type AppConfigPage, type AppPage } from '@grafana/plugin-e2e';
 import { type Locator } from '@playwright/test';
 
-import { ROUTES } from '../../src/constants';
 import pluginJson from '../../src/plugin.json';
 import { DEFAULT_TIMERANGE } from '../config/constants';
 import { SelectMetricView } from './views/SelectMetricView';
-import { UI_TEXT } from '../../src/constants/ui';
 
 type AppTestFixture = {
   appConfigPage: AppConfigPage;
@@ -14,7 +12,6 @@ type AppTestFixture = {
   otelSwitch: Locator;
   navigateToTrail: () => Promise<void>;
   getMetricPanel: (title: string) => Promise<Locator>;
-  forEachTest: void;
 };
 
 export const test = base.extend<AppTestFixture>({
@@ -24,16 +21,6 @@ export const test = base.extend<AppTestFixture>({
     });
     await use(configPage);
   },
-  forEachTest: [
-    async ({ page }, use) => {
-      // This code runs before every test.
-      await page.goto('http://localhost:8000');
-      await use();
-      // This code runs after every test.
-      console.log('Last URL:', page.url());
-    },
-    { auto: true },
-  ],
   gotoPage: async ({ gotoAppPage }, use) => {
     await use((path) =>
       gotoAppPage({
