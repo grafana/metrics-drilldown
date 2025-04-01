bffe9a4a60ec30160cf95b7cd1d42b9f583eb342
yangkb09 <yangkb09@gmail.com>
2025-04-01 15:49:22 -0400

    fix: linter error

--- src/WingmanDataTrail/MetricVizPanel/panels/heatmap.tsx
+++ src/WingmanDataTrail/MetricVizPanel/panels/heatmap.tsx
@@ -1,4 +1,5 @@
 import { PanelBuilders, type SceneObject, type SceneQueryRunner } from '@grafana/scenes';
+import { HeatmapColorMode } from '@grafana/schema/dist/esm/raw/composable/heatmap/panelcfg/x/HeatmapPanelCfg_types.gen';
 
 interface PanelProps {
   panelTitle: string;
@@ -13,9 +14,16 @@ export function buildHeatmapPanel({ panelTitle, queryRunner, color, headerAction
     PanelBuilders.heatmap()
       .setTitle(panelTitle)
       .setData(queryRunner)
-      .setColor({ mode: 'fixed', fixedColor: color })
+      .setOption('calculate', false)
+      .setOption('color', {
+        mode: HeatmapColorMode.Scheme,
+        exponent: 0.5,
+        scheme: 'Spectral',
+        steps: 32,
+        reverse: false,
+      })
       // we clone to prevent Scenes warnings "SceneObject already has a parent set that is different from the new parent. You cannot share the same SceneObject instance in multiple scenes or in multiple different places of the same scene graph. Use SceneObject.clone() to duplicate a SceneObject or store a state key reference and use sceneGraph.findObject to locate it."
       .setHeaderActions(headerActions.map((action) => action.clone()))
-      .setOption('legend', { showLegend: !hideLegend })
+      .setOption('legend', { show: !hideLegend })
   );
 }
