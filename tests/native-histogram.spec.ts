f51a1fac7f2b22dbf57692d1c6aa8136ce254aab
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-04-02 13:29:54 -0600

    native histogram banner flow

---
+++ tests/native-histogram.spec.ts
@@ -0,0 +1,17 @@
+import { test } from '../e2e/fixtures';
+
+const NATIVE_HISTOGRAM_LABEL = `grafana_database_all_migrations_duration_seconds`;
+
+test.describe('Native Histogram', () => {
+  test.beforeEach(async ({ selectMetricView }) => {
+    await selectMetricView.goto();
+  });
+
+  test('Banner Flow', async ({ selectMetricView }) => {
+    await selectMetricView.assertNativeHistogramBanner();
+    await selectMetricView.expandNativeHistogramBanner();
+    await selectMetricView.selectNativeHistogramExample(NATIVE_HISTOGRAM_LABEL);
+    await selectMetricView.assertPanel(NATIVE_HISTOGRAM_LABEL);
+    await selectMetricView.assertHeatmapLabel(NATIVE_HISTOGRAM_LABEL);
+  });
+});
