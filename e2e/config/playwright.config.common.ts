fbefb2dd993e69ddcd89477ab4c5e3c1a7390ac2
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-04-08 09:16:21 -0600

    re-add permissions

--- e2e/config/playwright.config.common.ts
+++ e2e/config/playwright.config.common.ts
@@ -83,6 +83,9 @@ export function config(config: CustomEnvConfig) {
         use: {
           ...devices['Desktop Chrome'],
           viewport: CHROMIUM_VIEWPORT,
+          storageState: 'playwright/.auth/admin.json',
+          // Used by the Copy URL test
+          permissions: ['clipboard-read', 'clipboard-write'],
         },
         dependencies: ['auth'],
       },
