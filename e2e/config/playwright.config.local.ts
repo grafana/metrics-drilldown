f6a0ea9c7146ed671d41ef3ec9f390be20c69b6b
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-04-09 17:10:58 -0600

    don't open playwright server locally
    this helps to iterate a bit faster

--- e2e/config/playwright.config.local.ts
+++ e2e/config/playwright.config.local.ts
@@ -3,6 +3,6 @@ import { config } from './playwright.config.common';
 export default config({
   reporter: [
     ['list', { printSteps: true }],
-    ['html', { outputFolder: '../test-reports', open: 'on-failure' }],
+    ['html', { outputFolder: '../test-reports', open: 'never' }],
   ],
 });
