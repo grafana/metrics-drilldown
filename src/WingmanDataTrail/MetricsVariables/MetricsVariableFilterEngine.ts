51184ed0afa5358b8925bf4d813f2c6895c88ba7
Kat Yang <69819079+yangkb09@users.noreply.github.com>
2025-04-10 16:19:34 -0400

    feat: add filter by metric suffixes to sidebar (#274)
    * feat: add metric suffix filters to sidebar - has bugs
    
    * chore: remove metric categories filters code
    
    * feat: remove <none> / catchall suffix category
    
    * chore: remove categories code that was reintroduced from merge main

--- src/WingmanDataTrail/MetricsVariables/MetricsVariableFilterEngine.ts
+++ src/WingmanDataTrail/MetricsVariables/MetricsVariableFilterEngine.ts
@@ -5,7 +5,7 @@ import { type MetricOptions } from './MetricsVariable';
 
 export type MetricFilters = {
   prefixes: string[];
-  categories: string[];
+  suffixes: string[];
   names: string[];
 };
 
@@ -14,7 +14,7 @@ export class MetricsVariableFilterEngine {
   private initOptions: VariableValueOption[] = [];
   private filters: MetricFilters = {
     prefixes: [],
-    categories: [],
+    suffixes: [],
     names: [],
   };
 
@@ -27,14 +27,14 @@ export class MetricsVariableFilterEngine {
   }
 
   public applyFilters(filters: Partial<MetricFilters> = this.filters, settings = { notify: true }) {
-    const updatedFilters = {
+    const updatedFilters: MetricFilters = {
       ...this.filters,
       ...filters,
     };
 
     if (
       isEqual(this.filters, updatedFilters) ||
-      (!updatedFilters.names.length && !updatedFilters.prefixes.length && !updatedFilters.categories.length)
+      (!updatedFilters.names.length && !updatedFilters.prefixes.length && !updatedFilters.suffixes.length)
     ) {
       this.filters = updatedFilters;
 
@@ -54,8 +54,8 @@ export class MetricsVariableFilterEngine {
       filteredOptions = MetricsVariableFilterEngine.applyPrefixFilters(filteredOptions, updatedFilters.prefixes);
     }
 
-    if (updatedFilters.categories.length > 0) {
-      filteredOptions = MetricsVariableFilterEngine.applyCategoriesFilters(filteredOptions, updatedFilters.categories);
+    if (updatedFilters.suffixes.length > 0) {
+      filteredOptions = MetricsVariableFilterEngine.applySuffixFilters(filteredOptions, updatedFilters.suffixes);
     }
 
     if (updatedFilters.names.length > 0) {
@@ -74,7 +74,7 @@ export class MetricsVariableFilterEngine {
   private static applyPrefixFilters(options: MetricOptions, prefixes: string[]): MetricOptions {
     const pattern = prefixes
       .map((prefix) => {
-        // catch-all (see computeMetricPrefixGroups)
+        // Multi-value support (see computeMetricPrefixGroups)
         if (prefix.includes('|')) {
           return `${prefix
             .split('|')
@@ -91,15 +91,24 @@ export class MetricsVariableFilterEngine {
     return options.filter((option) => prefixesRegex.test(option.value as string));
   }
 
-  private static applyCategoriesFilters(options: MetricOptions, categories: string[]): MetricOptions {
-    let filteredOptions: MetricOptions = [];
+  private static applySuffixFilters(options: MetricOptions, suffixes: string[]): MetricOptions {
+    const pattern = suffixes
+      .map((suffix) => {
+        // Multi-value support (see computeMetricSuffixGroups)
+        if (suffix.includes('|')) {
+          return `${suffix
+            .split('|')
+            .map((s) => `^${s}$`)
+            .join('|')}`;
+        }
 
-    for (const category of categories) {
-      const categoryRegex = MetricsVariableFilterEngine.buildRegex(category, 'i'); // see computeMetricCategories
-      filteredOptions = filteredOptions.concat(options.filter((option) => categoryRegex.test(option.value)));
-    }
+        return `.+${suffix}$`;
+      })
+      .join('|');
 
-    return filteredOptions;
+    const suffixesRegex = MetricsVariableFilterEngine.buildRegex(`(${pattern})`);
+
+    return options.filter((option) => suffixesRegex.test(option.value as string));
   }
 
   private static applyNamesFilters(options: MetricOptions, names: string[]): MetricOptions {
