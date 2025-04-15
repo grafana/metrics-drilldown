4c707afdad750e3a0fb9c945ec44bdd9a5a70848
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-04-15 23:30:08 +0200

    fix(Filters): Fix prefix & suffix regexes

--- src/WingmanDataTrail/MetricsVariables/MetricsVariableFilterEngine.ts
+++ src/WingmanDataTrail/MetricsVariables/MetricsVariableFilterEngine.ts
@@ -101,11 +101,11 @@ export class MetricsVariableFilterEngine {
         if (prefix.includes('|')) {
           return `${prefix
             .split('|')
-            .map((p) => `^${p}$`)
+            .map((p) => `^${p}([^a-z0-9]|$)`)
             .join('|')}`;
         }
 
-        return `^${prefix}.+`;
+        return `^${prefix}([^a-z0-9]|$)`;
       })
       .join('|');
 
@@ -121,11 +121,11 @@ export class MetricsVariableFilterEngine {
         if (suffix.includes('|')) {
           return `${suffix
             .split('|')
-            .map((s) => `^${s}$`)
+            .map((s) => `${s}$`)
             .join('|')}`;
         }
 
-        return `.+${suffix}$`;
+        return `${suffix}$`;
       })
       .join('|');
 
