08d773c7f499df6ec0a83a2fe1511be2f205c6cd
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-04-23 11:47:57 -0600

    remove bookmarks from homepage

--- src/DataTrailsHome.tsx
+++ src/DataTrailsHome.tsx
@@ -2,14 +2,13 @@ import { css } from '@emotion/css';
 import { type GrafanaTheme2 } from '@grafana/data';
 import { SceneObjectBase, type SceneComponentProps, type SceneObjectState } from '@grafana/scenes';
 import { Box, Button, Icon, Stack, Text, TextLink, useStyles2, useTheme2 } from '@grafana/ui';
-import React, { useState } from 'react';
+import React from 'react';
 
 import { testIds } from 'App/testIds';
 import { UI_TEXT } from 'constants/ui';
 
 import { DarkModeRocket, LightModeRocket } from './assets/rockets';
 import { type DataTrail } from './DataTrail';
-import { DataTrailsBookmarks } from './DataTrailBookmarks';
 import { DataTrailsRecentMetrics } from './DataTrailsRecentMetrics';
 import { reportExploreMetrics } from './interactions';
 import { getTrailStore } from './TrailStore/TrailStore';
@@ -45,15 +44,8 @@ export class DataTrailsHome extends SceneObjectBase<DataTrailsHomeState> {
   };
 
   static Component = ({ model }: SceneComponentProps<DataTrailsHome>) => {
-    const [_, setLastDelete] = useState(Date.now());
     const styles = useStyles2(getStyles);
     const theme = useTheme2();
-
-    const onDelete = (index: number) => {
-      getTrailStore().removeBookmark(index);
-      reportExploreMetrics('bookmark_changed', { action: 'deleted' });
-      setLastDelete(Date.now()); // trigger re-render
-    };
 
     return (
       <article className={styles.container} data-testid={testIds.pageHome.container}>
@@ -89,7 +81,6 @@ export class DataTrailsHome extends SceneObjectBase<DataTrailsHomeState> {
           </Stack>
         </section>
         <DataTrailsRecentMetrics onSelect={model.onSelectRecentTrail} />
-        <DataTrailsBookmarks onSelect={model.onSelectBookmark} onDelete={onDelete} />
       </article>
     );
   };
