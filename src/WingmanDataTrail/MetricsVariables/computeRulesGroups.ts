0e0329461c1c50fc98915f49369cabe9cc09b80e
Marc M. <146180665+grafakus@users.noreply.github.com>
2025-04-24 12:11:25 +0200

    feat(SideBar): Remove alerting rules filter (#325)

--- src/WingmanDataTrail/MetricsVariables/computeRulesGroups.ts
+++ src/WingmanDataTrail/MetricsVariables/computeRulesGroups.ts
@@ -1,23 +1,16 @@
 import { ruleGroupLabels } from './metricLabels';
 
-type MetricType = 'metrics' | 'rules' | 'alerts';
+type MetricType = 'metrics' | 'rules';
 
 export function computeRulesGroups(options: Array<{ label: string; value: string }>) {
   const rulesMap = new Map<MetricType, string[]>([
     ['metrics', []],
     ['rules', []],
-    ['alerts', []],
   ]);
 
   for (const option of options) {
     const { value } = option;
-    let key: MetricType = 'metrics';
-
-    if (/:/i.test(value)) {
-      key = 'rules';
-    } else if (/^alert/i.test(value)) {
-      key = 'alerts';
-    }
+    const key: MetricType = /:/i.test(value) ? 'rules' : 'metrics';
 
     const values = rulesMap.get(key) ?? [];
     values.push(value);
@@ -25,8 +18,7 @@ export function computeRulesGroups(options: Array<{ label: string; value: string
   }
 
   return [
-    { value: '^(?!alert)(?!.*:.*)', label: ruleGroupLabels.metrics, count: rulesMap.get('metrics')!.length },
+    { value: '^(?!.*:.*)', label: ruleGroupLabels.metrics, count: rulesMap.get('metrics')!.length },
     { value: ':', label: ruleGroupLabels.rules, count: rulesMap.get('rules')!.length },
-    { value: '^alert', label: ruleGroupLabels.alerts, count: rulesMap.get('alerts')!.length },
   ];
 }
