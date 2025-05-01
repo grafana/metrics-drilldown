c3604737abb642df5ccc8adf2a128f40d9a753f7
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-05-01 18:43:39 +0200

    fix: Address sonarjs/no-nested-functions

--- src/autoQuery/getAutoQueriesForMetric.test.ts
+++ src/autoQuery/getAutoQueriesForMetric.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable sonarjs/no-nested-functions */
+
 import { VAR_FILTERS_EXPR, VAR_METRIC_EXPR, VAR_OTEL_JOIN_QUERY_EXPR } from '../shared';
 import { getAutoQueriesForMetric } from './getAutoQueriesForMetric';
 import { generateBaseQuery } from './queryGenerators/baseQuery';
