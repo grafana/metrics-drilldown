3f92008479f351bdbd09f3b82e571cb3f34bb679
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-05-27 17:56:20 +0200

    fix(E2E): Fix MetricScene view tests

--- e2e/tests/metric-scene-view.spec.ts
+++ e2e/tests/metric-scene-view.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '../fixtures';
 
-const METRIC_NAME = 'handler_duration_seconds_count';
+const METRIC_NAME = 'target_info';
 const URL_SEARCH_PARAMS_WITH_METRIC_NAME = new URLSearchParams([['metric', METRIC_NAME]]);
 
 test.describe('Metrics Scene view', () => {
