fe19f375d3418f62f2a93368ba66e550d3748616
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-06-17 20:02:49 +0200

    fix(MetricScene): Prevent high cardinality labels to make the UI unresponsive

--- src/Breakdown/MetricLabelsList/transformations/addRefId.ts
+++ src/Breakdown/MetricLabelsList/transformations/addRefId.ts
@@ -1,6 +1,12 @@
 import { type DataFrame } from '@grafana/data';
-import { merge } from 'lodash';
 import { map, type Observable } from 'rxjs';
 
 export const addRefId = () => (source: Observable<DataFrame[]>) =>
-  source.pipe(map((data: DataFrame[]) => data?.map((d, i) => merge(d, { refId: `${d.refId}-${i}` }))));
+  source.pipe(
+    map((data: DataFrame[]) =>
+      data?.map((d, i) => {
+        d.refId = `${d.refId}-${i}`;
+        return d;
+      })
+    )
+  );
