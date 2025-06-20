a8b6448141e584c440b98503f9427b8924042c5f
Nick Richmond <nick.richmond@grafana.com>
2025-06-20 17:17:23 -0400

    feat: add basic exposed component

--- src/module.tsx
+++ src/module.tsx
@@ -2,6 +2,7 @@ import { AppPlugin, type AppRootProps } from '@grafana/data';
 import { LoadingPlaceholder } from '@grafana/ui';
 import React, { lazy, Suspense } from 'react';
 
+import { LabelBreakdown } from 'exposedComponents';
 import { linkConfigs } from 'extensions/links';
 import { logger } from 'tracking/logger/logger';
 
@@ -28,3 +29,10 @@ export const plugin = new AppPlugin<{}>().setRootPage(App);
 for (const linkConfig of linkConfigs) {
   plugin.addLink(linkConfig);
 }
+
+plugin.exposeComponent({
+  id: 'grafana-metricsdrilldown-app/breakdown-extension/v1',
+  title: 'Label Breakdown',
+  description: 'A metrics label breakdown view from the Metrics Drilldown app.',
+  component: LabelBreakdown,
+});
