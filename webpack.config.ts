464ff1544d96c71d3d431c6ac16665bd3c23e159
Nick Richmond <nick.richmond@grafana.com>
2025-07-18 11:23:53 -0400

    chore: remove other live reload plugin config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,10 @@
 import { type Configuration } from 'webpack';
-import LiveReloadPlugin from 'webpack-livereload-plugin';
 import { merge } from 'webpack-merge';
 
 import grafanaConfig from './.config/webpack/webpack.config';
 
 const config = async (env): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
-
-  // Get the live reload port from environment variable or use default
-  const liveReloadPort = process.env.WEBPACK_LIVERELOAD_PORT
-    ? parseInt(process.env.WEBPACK_LIVERELOAD_PORT, 10)
-    : 35729;
-
-  // Filter out the original LiveReloadPlugin from base config to avoid conflicts
-  if (baseConfig.plugins && env.development) {
-    baseConfig.plugins = baseConfig.plugins.filter((plugin) => !(plugin instanceof LiveReloadPlugin));
-  }
 
   return merge(baseConfig, {
     externals: ['react-router'],
@@ -26,12 +15,6 @@ const config = async (env): Promise<Configuration> => {
     output: {
       asyncChunks: true,
     },
-    plugins: [
-      new LiveReloadPlugin({
-        port: liveReloadPort,
-        hostname: 'localhost',
-      }),
-    ],
   });
 };
 
