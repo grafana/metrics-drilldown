abe7fadfe708ab18e31e7ce3e24544da5dfbc8b5
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-08-06 20:10:50 +0200

    test(EndToEnd): Update Grafana supported versions (12..0.3 => 12.0.4)

--- e2e/config/grafana-versions-supported.ts
+++ e2e/config/grafana-versions-supported.ts
@@ -8,6 +8,6 @@
 // TODO: automate (see https://github.com/grafana/plugin-actions/blob/main/e2e-version/README.md)
 export const GRAFANA_VERSIONS_SUPPORTED = [
   { image: 'grafana-enterprise', version: '12.1.0' },
-  { image: 'grafana-enterprise', version: '12.0.3' },
+  { image: 'grafana-enterprise', version: '12.0.4' },
   { image: 'grafana-enterprise', version: '11.6.4' },
 ];
