3bbdfc80fe5b6b7db01d4bd7a8027ade2dc018c6
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-08-20 12:07:42 +0200

    perf(*): Add knip to detect and remove unused code

---
+++ knip.js
@@ -0,0 +1,37 @@
+export default {
+  entry: ['src/App/App.tsx'],
+  project: ['src/**/*.{js,ts}'],
+  ignore: ['src/mocks/**', 'src/stubs/**'],
+  ignoreDependencies: [
+    // dependencies
+    '@grafana/lezer-logql',
+    '@lezer/common',
+    // devDependencies
+    '@grafana/eslint-config',
+    '@grafana/plugin-e2e',
+    '@stylistic/eslint-plugin-ts',
+    '@swc/helpers',
+    '@testing-library/jest-dom',
+    '@types/testing-library__jest-dom',
+    'copy-webpack-plugin',
+    'dotenv',
+    'eslint-config-prettier',
+    'eslint-plugin-deprecation',
+    'eslint-plugin-jsdoc',
+    'eslint-plugin-react',
+    'eslint-plugin-react-hooks',
+    'eslint-webpack-plugin',
+    'fork-ts-checker-webpack-plugin',
+    'glob',
+    'replace-in-file-webpack-plugin',
+    'terser-webpack-plugin',
+    'ts-node',
+    'webpack-livereload-plugin',
+    'webpack-subresource-integrity',
+    'webpack-virtual-modules',
+    // unlisted dependencies
+    '@jest/globals',
+    'lodash',
+    'webpack-merge',
+  ],
+};
