d31d9c4aeba3bf2ad436e004be4c3270e23ef288
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-09-04 11:29:23 +0200

    chore: Improve comment

--- src/GmdVizPanel/matchers/getPanelTypeForMetric.ts
+++ src/GmdVizPanel/matchers/getPanelTypeForMetric.ts
@@ -5,6 +5,10 @@ import { type PanelType } from 'GmdVizPanel/types/available-panel-types';
 import { isClassicHistogramMetric } from './isClassicHistogramMetric';
 import { isStatusUpDownMetric } from './isStatusUpDownMetric';
 
+/**
+ * These are functions that receive a metric name to determine in which panel type they should be displayed.
+ * Note that they don't consider user preferences stored in user storage.
+ */
 export async function getPanelTypeForMetric(metric: string, dataTrail: DataTrail): Promise<PanelType> {
   if (isStatusUpDownMetric(metric)) {
     return 'statushistory';
