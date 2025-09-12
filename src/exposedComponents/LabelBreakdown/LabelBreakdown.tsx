24cb577ddd6fe35cb994c6c8430fce7f23785b26
Nick Richmond <nick.richmond@grafana.com>
2025-09-12 18:16:47 -0400

    fix: async parsing in exposed component

--- src/exposedComponents/LabelBreakdown/LabelBreakdown.tsx
+++ src/exposedComponents/LabelBreakdown/LabelBreakdown.tsx
@@ -1,11 +1,12 @@
 import { type DataSourceApi } from '@grafana/data';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { ErrorView } from 'App/ErrorView';
 import { Wingman } from 'App/Routes';
 import { useCatchExceptions } from 'App/useCatchExceptions';
 import { reportExploreMetrics } from 'interactions';
 import { newMetricsTrail } from 'utils';
+import { type ParsedPromQLQuery } from 'utils/utils.promql';
 
 import { parsePromQLQuery } from '../../extensions/links';
 import { toSceneTimeRange } from '../../utils/utils.timerange';
@@ -17,13 +18,30 @@ export interface LabelBreakdownProps {
   dataSource: DataSourceApi;
 }
 
-const LabelBreakdown = async ({ query, initialStart, initialEnd, dataSource }: LabelBreakdownProps) => {
+const LabelBreakdown = ({ query, initialStart, initialEnd, dataSource }: LabelBreakdownProps) => {
   const [error] = useCatchExceptions();
-  const { metric, labels } = await parsePromQLQuery(query);
+  const [parsedPromQLQuery, setParsedPromQLQuery] = useState<ParsedPromQLQuery | null>(null);
+  const initRef = useRef(false);
+
+  useEffect(() => {
+    if (!initRef.current) {
+      initRef.current = true;
+      reportExploreMetrics('exposed_component_viewed', { component: 'label_breakdown' });
+    }
+  }, []);
+
+  useEffect(() => {
+    parsePromQLQuery(query).then(setParsedPromQLQuery);
+  }, [query]);
+
+  if (!parsedPromQLQuery) {
+    return <div>Loading...</div>;
+  }
+
   const trail = newMetricsTrail({
-    metric,
+    metric: parsedPromQLQuery.metric,
     initialDS: dataSource.uid,
-    initialFilters: labels.map(({ label, op, value }) => ({
+    initialFilters: parsedPromQLQuery.labels.map(({ label, op, value }) => ({
       key: label,
       operator: op,
       value,
@@ -31,14 +49,6 @@ const LabelBreakdown = async ({ query, initialStart, initialEnd, dataSource }: L
     $timeRange: toSceneTimeRange(initialStart, initialEnd),
     embedded: true,
   });
-
-  const initRef = useRef(false);
-  useEffect(() => {
-    if (!initRef.current) {
-      initRef.current = true;
-      reportExploreMetrics('exposed_component_viewed', { component: 'label_breakdown' });
-    }
-  }, []);
 
   return (
     <div data-testid="metrics-drilldown-embedded-label-breakdown">
--- src/exposedComponents/LabelBreakdown/LazyLabelBreakdown.tsx
+++ src/exposedComponents/LabelBreakdown/LazyLabelBreakdown.tsx
@@ -1,25 +1,11 @@
-import React, { lazy, Suspense, useEffect, useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { type LabelBreakdownProps } from './LabelBreakdown';
 
-const LabelBreakdownAsync = lazy(async () => {
-  const module = await import('./LabelBreakdown');
-  
-  const AsyncWrapper = (props: LabelBreakdownProps): React.ReactElement => {
-    const [Component, setComponent] = useState<React.ReactElement>(<div>Loading...</div>);
-    
-    useEffect(() => {
-      module.default(props).then(element => setComponent(element as React.ReactElement));
-    }, [props]);
-    
-    return Component;
-  };
-  
-  return { default: AsyncWrapper };
-});
+const LabelBreakdown = lazy(() => import('./LabelBreakdown'));
 
 export const LazyLabelBreakdown = (props: LabelBreakdownProps) => (
   <Suspense fallback={<div>Loading...</div>}>
-    <LabelBreakdownAsync {...props} />
+    <LabelBreakdown {...props} />
   </Suspense>
 );
