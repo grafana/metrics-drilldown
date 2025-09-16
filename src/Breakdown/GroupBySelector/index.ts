f9f9d28f66f5ef3498e0f3ff60597b8502f84d42
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-09-16 17:02:44 -0600

    simplify exports

--- src/Breakdown/GroupBySelector/index.ts
+++ src/Breakdown/GroupBySelector/index.ts
@@ -1,12 +1,2 @@
-export {
-  filteredOptions,
-  getModifiedSelectOptions,
-  GroupBySelector,
-  removeAttributePrefixes,
-} from './GroupBySelector';
-export type {
-  AttributePrefixConfig,
-  GroupBySelectorProps,
-  SearchConfig,
-} from './GroupBySelector';
+export { GroupBySelector } from './GroupBySelector';
 export { VariableBackedGroupBySelector } from './VariableAdapter';
