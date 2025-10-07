9739a8b2fda6011091430f8be0467e71e12a251c
Nick Richmond <nick.richmond@grafana.com>
2025-10-07 09:37:51 -0400

    feat: metric name in nav breadcrumbs

--- src/App/Trail.tsx
+++ src/App/Trail.tsx
@@ -1,21 +1,41 @@
+import { PageLayoutType, type NavModelItem } from '@grafana/data';
+import { PluginPage } from '@grafana/runtime';
 import { UrlSyncContextProvider } from '@grafana/scenes';
 import React from 'react';
 
 import { type DataTrail } from 'AppDataTrail/DataTrail';
+import { MetricScene } from 'MetricScene/MetricScene';
 
 type TrailProps = {
   trail: DataTrail;
 };
 
 export default function Trail({ trail }: Readonly<TrailProps>) {
+  const { topScene, metric } = trail.useState();
+
+  let pageNav: NavModelItem | undefined = undefined;
+
+  if (metric && topScene instanceof MetricScene) {
+    pageNav = {
+      text: metric, // add selected metric name to navigation breadcrumbs
+      url: window.location.pathname + window.location.search,
+      parentItem: {
+        text: 'Metrics',
+        url: window.location.pathname,
+      },
+    };
+  }
+
   return (
-    <UrlSyncContextProvider
-      scene={trail}
-      createBrowserHistorySteps={true}
-      updateUrlOnInit={true}
-      namespace={trail.state.urlNamespace}
-    >
-      <trail.Component model={trail} />
-    </UrlSyncContextProvider>
+    <PluginPage pageNav={pageNav} layout={PageLayoutType.Canvas}>
+      <UrlSyncContextProvider
+        scene={trail}
+        createBrowserHistorySteps={true}
+        updateUrlOnInit={true}
+        namespace={trail.state.urlNamespace}
+      >
+        <trail.Component model={trail} />
+      </UrlSyncContextProvider>
+    </PluginPage>
   );
 }
