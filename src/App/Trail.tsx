dab363d5657afffd8857330cde444205c3d9fe41
Brendan O'Handley <brendan.ohandley@grafana.com>
2025-10-10 13:02:45 -0500

    feat: grafana assistant quick start questions (#774)
    * install assistant
    
    * add foundation for quickstart with readme(WIP)
    
    * add questions for the metrics reducer
    
    * test questions in App for metrics reducer
    
    * fix url matches for actual metrics drilldown url
    
    * simpler: focus on one set of questions
    
    * add context for grafana assistant to help with questions
    
    * trying to force to remove file
    
    * remove duplicated code

--- src/App/Trail.tsx
+++ src/App/Trail.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 
 import { type DataTrail } from 'AppDataTrail/DataTrail';
 
+import { useMetricsDrilldownQuestions } from './assistant/useMetricsDrilldownQuestions';
+
 type TrailProps = {
   trail: DataTrail;
 };
 
 export default function Trail({ trail }: Readonly<TrailProps>) {
+  // Register all assistant questions for metrics drilldown
+  // Questions are automatically matched based on URL patterns
+  useMetricsDrilldownQuestions();
+  
+
   return (
     <UrlSyncContextProvider
       scene={trail}
