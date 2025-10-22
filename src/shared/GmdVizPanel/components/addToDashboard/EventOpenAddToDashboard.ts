b569e636b94e3e85f3b706e378df42bdc427a258
Brendan O'Handley <brendan.ohandley@grafana.com>
2025-10-22 17:17:10 -0500

    add event for opening the modal

---
+++ src/shared/GmdVizPanel/components/addToDashboard/EventOpenAddToDashboard.ts
@@ -0,0 +1,12 @@
+import { BusEventWithPayload } from '@grafana/data';
+
+import { type PanelDataRequestPayload } from './addToDashboard';
+
+interface EventOpenAddToDashboardPayload {
+  panelData: PanelDataRequestPayload;
+}
+
+export class EventOpenAddToDashboard extends BusEventWithPayload<EventOpenAddToDashboardPayload> {
+  public static readonly type = 'open-add-to-dashboard';
+}
+
