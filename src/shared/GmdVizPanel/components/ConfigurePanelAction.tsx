ad85acfb5738fb0d296eac7d667c3cc010552213
yangkb09 <yangkb09@gmail.com>
2025-11-04 13:36:03 -0500

    chore: remove margin and no-effect margin-left from configure panel settings cog

--- src/shared/GmdVizPanel/components/ConfigurePanelAction.tsx
+++ src/shared/GmdVizPanel/components/ConfigurePanelAction.tsx
@@ -63,9 +63,7 @@ export class ConfigurePanelAction extends SceneObjectBase<ConfigurePanelActionSt
 
 const getStyles = (theme: GrafanaTheme2) => ({
   selectButton: css`
-    margin: 0;
     padding: 0;
-    margin-left: ${theme.spacing(1)};
   `,
   active: css`
     color: ${theme.colors.text.maxContrast};
