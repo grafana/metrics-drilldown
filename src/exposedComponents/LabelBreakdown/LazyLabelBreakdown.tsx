37c1d7ad0fd39290f7a5292d6132337a6e8813ef
Nick Richmond <nick.richmond@grafana.com>
2025-11-26 09:51:37 -0500

    fix: init scenes i18n

--- src/exposedComponents/LabelBreakdown/LazyLabelBreakdown.tsx
+++ src/exposedComponents/LabelBreakdown/LazyLabelBreakdown.tsx
@@ -2,7 +2,14 @@ import React, { lazy, Suspense } from 'react';
 
 import { type LabelBreakdownProps } from './LabelBreakdown';
 
-const LabelBreakdown = lazy(() => import('./LabelBreakdown'));
+const LabelBreakdown = lazy(async () => {
+  // Initialize i18n for scenes library
+  const { initPluginTranslations } = await import('@grafana/i18n');
+  const { loadResources } = await import('@grafana/scenes');
+  await initPluginTranslations('grafana-scenes', [loadResources]);
+
+  return import('./LabelBreakdown');
+});
 
 export const LazyLabelBreakdown = (props: LabelBreakdownProps) => (
   <Suspense fallback={<div>Loading...</div>}>
