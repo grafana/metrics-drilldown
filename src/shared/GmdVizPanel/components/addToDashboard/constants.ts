ff776780391e90e84b7211c564ba74c7a07ef42d
Nic Westvold <nic.westvold@grafana.com>
2025-12-15 16:44:51 -0500

    chore: replace hardcoded constant with enum

--- src/shared/GmdVizPanel/components/addToDashboard/constants.ts
+++ src/shared/GmdVizPanel/components/addToDashboard/constants.ts
@@ -1,5 +1,5 @@
-// TODO: Use `PluginExtensionExposedComponents.AddToDashboardFormV1` when it becomes available from grafana
-export const ADD_TO_DASHBOARD_COMPONENT_ID = 'grafana/add-to-dashboard-form/v1';
+import { PluginExtensionExposedComponents } from '@grafana/data';
+
+export const ADD_TO_DASHBOARD_COMPONENT_ID = PluginExtensionExposedComponents.AddToDashboardFormV1;
 
 export const ADD_TO_DASHBOARD_LABEL = 'Add to dashboard';
-
