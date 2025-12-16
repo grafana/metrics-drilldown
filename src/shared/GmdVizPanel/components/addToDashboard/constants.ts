ee89a22bdad7cc387596407dea6efead4ac019b1
Nic Westvold <nic.westvold@grafana.com>
2025-12-16 12:04:13 -0500

    chore: ensure backwards compatibility with versions pre 12.3.0

--- src/shared/GmdVizPanel/components/addToDashboard/constants.ts
+++ src/shared/GmdVizPanel/components/addToDashboard/constants.ts
@@ -1,5 +1,6 @@
 import { PluginExtensionExposedComponents } from '@grafana/data';
 
-export const ADD_TO_DASHBOARD_COMPONENT_ID = PluginExtensionExposedComponents.AddToDashboardFormV1;
+export const ADD_TO_DASHBOARD_COMPONENT_ID =
+  PluginExtensionExposedComponents?.AddToDashboardFormV1 || 'grafana/add-to-dashboard-form/v1';
 
 export const ADD_TO_DASHBOARD_LABEL = 'Add to dashboard';
