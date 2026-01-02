d577db641f2253a57ef092e9d837b6502cd2c48f
Brendan O'Handley <brendan.ohandley@grafana.com>
2026-01-02 14:37:55 -0600

    testing a mini breakdown

--- src/exposedComponents/MiniBreakdown/MiniBreakdown.tsx
+++ src/exposedComponents/MiniBreakdown/MiniBreakdown.tsx
@@ -1,10 +1,12 @@
+import { css } from '@emotion/css';
+import { type GrafanaTheme2 } from '@grafana/data';
+import { useStyles2 } from '@grafana/ui';
 import React, { useEffect, useRef } from 'react';
 
 import { ErrorView } from 'App/ErrorView';
 import { useCatchExceptions } from 'App/useCatchExceptions';
-import { logger } from 'shared/logger/logger';
+import { MiniBreakdownScene } from 'exposedComponents/MiniBreakdown/MiniBreakdownScene';
 import { reportExploreMetrics } from 'shared/tracking/interactions';
-import { newMetricsTrail } from 'shared/utils/utils';
 
 import { parsePromQLQuery } from '../../extensions/links';
 import { toSceneTimeRange } from '../../shared/utils/utils.timerange';
@@ -13,39 +15,48 @@ export interface MiniBreakdownProps {
   query: string;
   initialStart: string | number;
   initialEnd: string | number;
-  dataSource: string; // The data source UID
+  dataSource: string;
 }
 
 export const MiniBreakdown = ({ query, initialStart, initialEnd, dataSource }: MiniBreakdownProps) => {
   const [error] = useCatchExceptions();
+  const styles = useStyles2(getStyles);
   const initRef = useRef(false);
 
   useEffect(() => {
     if (!initRef.current) {
       initRef.current = true;
-      reportExploreMetrics('exposed_component_viewed', { component: 'label_breakdown' });
+      reportExploreMetrics('exposed_component_viewed', { component: 'mini_breakdown' });
     }
   }, []);
 
   const { metric, labels } = parsePromQLQuery(query);
 
-  const trail = newMetricsTrail({
+  const scene = new MiniBreakdownScene({
     metric,
-    initialDS: dataSource,
+    dataSource,
     initialFilters: labels.map(({ label, op, value }) => ({
       key: label,
       operator: op,
       value,
     })),
     $timeRange: toSceneTimeRange(initialStart, initialEnd),
-    embedded: true,
   });
 
-  logger.log('trail', trail);
-
   return (
-    <div data-testid="metrics-drilldown-mini-label-breakdown">
-      {error ? <ErrorView error={error} /> : <>Hello world</>}
+    <div className={styles.container} data-testid="mini-breakdown">
+      {error ? <ErrorView error={error} /> : <scene.Component model={scene} />}
     </div>
   );
 };
+
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    container: css({
+      backgroundColor: theme.colors.background.primary,
+      borderRadius: theme.shape.radius.default,
+      padding: theme.spacing(1),
+      overflow: 'hidden',
+    }),
+  };
+}
