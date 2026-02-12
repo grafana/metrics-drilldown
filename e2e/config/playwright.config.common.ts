6ae7e1f71466fd67e310813984237b15b3b6d47c
Miguel Palau Zarza <mpalauzarza@gmail.com>
2026-02-11 18:15:13 -0600

    add a default port

--- e2e/config/playwright.config.common.ts
+++ e2e/config/playwright.config.common.ts
@@ -33,7 +33,9 @@ export function getGrafanaVersion(versionType: VersionType = 'patch') {
 }
 
 export function getGrafanaUrl(options: GetGrafanaUrlOptions = {}) {
-  const port = options.withScopes ? process.env.GRAFANA_SCOPES_PORT : process.env.GRAFANA_PORT;
+  const port = options.withScopes
+    ? (process.env.GRAFANA_SCOPES_PORT ?? '3002')
+    : (process.env.GRAFANA_PORT ?? '3001');
   return `http://localhost:${port}`;
 }
 
