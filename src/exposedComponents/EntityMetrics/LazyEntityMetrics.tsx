5067f05ddaa1f16437ebadf0a6a86fb499d502ef
Miguel Palau Zarza <mpalauzarza@gmail.com>
2026-02-16 17:31:04 -0600

    get it working again

--- src/exposedComponents/EntityMetrics/LazyEntityMetrics.tsx
+++ src/exposedComponents/EntityMetrics/LazyEntityMetrics.tsx
@@ -1,11 +1,25 @@
+import { Trans } from '@grafana/i18n';
 import React, { lazy, Suspense } from 'react';
 
 import { type EntityMetricsProps } from './EntityMetrics';
 
-const EntityMetrics = lazy(() => import('./EntityMetrics'));
+const EntityMetrics = lazy(async () => {
+  // Initialize i18n for scenes library before loading the component
+  const { initPluginTranslations } = await import('@grafana/i18n');
+  const { loadResources } = await import('@grafana/scenes');
+  await initPluginTranslations('grafana-scenes', [loadResources]);
+
+  return import('./EntityMetrics');
+});
 
 export const LazyEntityMetrics = (props: EntityMetricsProps) => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense
+    fallback={
+      <div>
+        <Trans i18nKey="lazy-loading.fallback">Loading...</Trans>
+      </div>
+    }
+  >
     <EntityMetrics {...props} />
   </Suspense>
 );
