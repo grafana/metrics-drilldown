79306e3426df70ce3879c9c715d5c4feb9ac4f2b
Brendan O'Handley <brendan.ohandley@grafana.com>
2026-09-18 15:48:21 -0500

    add todos dated a month out to remove gate checks

--- src/AppDataTrail/DataTrail.tsx
+++ src/AppDataTrail/DataTrail.tsx
@@ -708,8 +708,8 @@ function isScopesSupported(): boolean {
   return Boolean(
     config.featureToggles.scopeFilters &&
     config.featureToggles.enableScopesInMetricsExplore &&
-    // Scopes support in Grafana appears to begin with Grafana 12.0.0. We can remove
-    // the version check once the `dependencies.grafanaDependency` is updated to 12.0.0 or higher.
+    // Scopes support in Grafana appears to begin with Grafana 12.0.0.
+    // TODO(2026-10-18): grafanaDependency is now >=13.1.0, so this is always true. Remove.
     !config.buildInfo.version.startsWith('11.')
   );
 }
